edce36684425a8036a96aad5dac21f0b21f03bdc
Mmackz <matthewmackenz@gmail.com>
2022-03-21 19:01:39 -0700

    ✨ mobile specific functions

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const test = vhCheck();
+// sets proper fullscreen height for mobile
+vhCheck();
 
-console.log("Hi")
-console.log(test);
+// scrolls to bottom to ensure form is visible when filling inputs
+document.querySelectorAll("input").forEach((input) => {
+   if (!["confirm", "password"].includes(input.id)) {
+      input.addEventListener("focus", () => {
+         setTimeout(() => {
+            window.scrollBy(0, window.innerHeight);
+         }, 150)   
+      })
+   }
+})
