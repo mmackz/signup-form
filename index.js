70d795310c325b41fe1834cb60cac2298638389f
Mmackz <matthewmackenz@gmail.com>
2022-03-22 13:23:36 -0700

    🎨 add background blur filter

--- index.js
+++ index.js
@@ -7,7 +7,27 @@ document.querySelectorAll("input").forEach((input) => {
       input.addEventListener("focus", () => {
          setTimeout(() => {
             window.scrollBy(0, window.innerHeight);
-         }, 150)   
-      })
+         }, 150);
+      });
    }
-})
+});
+
+window.addEventListener("resize", () => {
+   const main = document.querySelector("main");
+   const size = main.getBoundingClientRect();
+   const blur = document.querySelector(".blur");
+   const blurSize = blur.getBoundingClientRect();
+   const topPercent = (size.top / blurSize.height) * 100 + "%";
+   const leftPercent = (size.left / blurSize.width) * 100 + "%";
+   const bottomPercent = (size.bottom / blurSize.height) * 100 + "%";
+   const rightPercent = (size.right / blurSize.width) * 100 + "%";
+   console.log(blurSize);
+   console.log(size);
+   console.log(leftPercent);
+   document
+      .querySelector(":root")
+      .style.setProperty(
+         "--mask",
+         `polygon(0% 0%, 0% 100%, ${leftPercent} 100%, ${leftPercent} ${topPercent}, ${rightPercent} ${topPercent}, ${rightPercent} ${bottomPercent}, ${leftPercent} ${bottomPercent}, ${leftPercent} 100%, 100% 100%, 100% 0%)`
+      );
+});
